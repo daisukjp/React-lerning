bc47957cee4d4fb027e0d1f7f5d34c724f023dd1
Daisuke Sato <dsato2@nait.ca>
2022-08-24 23:59:09 -0600

    added project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# React-lerning
+# License
+The source code is licensed MIT.
+
+## 概要
+[新・日本一わかりやすいReact入門基礎編](https://youtube.com/playlist?list=PLX8Rsrpnn3IWPoM7-1YPDksRRkamRY25k)のサンプルコードです。
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {BadCounter, Counter, TextInput, ToggleButton} from "./components";
+
+function App() {
+  return (
+    <div>
+      {/*<TextInput />*/}
+      {/*<BadCounter />*/}
+      <Counter />
+      {/* <ToggleButton /> */}
+    </div>
+  );
+}
+
+export default App;
